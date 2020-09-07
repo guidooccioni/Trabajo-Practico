7ad16668c7c53979ef5454537e5f4ae16e587ad7
guidooccioni <67933711+guidooccioni@users.noreply.github.com>
2020-09-07 15:50:29 -0300

    Create LIB_TP.h

---
+++ LIB_TP.h
@@ -0,0 +1,27 @@
+#ifndef LIB_TP
+#define LIB_TP
+
+#include <stdio.h>
+
+typedef enum {
+  mantener = 0,
+  reducir = 1
+}acciones_t;
+
+typedef struct {
+  float Vset;       // tensión seteada
+  float Imax;       // corriente máxima
+  float Vo;        // tensión que entrega la fuente
+  float Io;        // corriente de la fuente
+  float Rset;       // resistencia seteada
+}Valores_t;
+
+Valores_t cargaVI(Valores_t val);
+
+Valores_t cargaR(Valores_t val);
+
+int disminuir(acciones_t);
+
+
+
+#endif
