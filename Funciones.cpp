9af9569fe70713c745ac5354f618687d345b410e
guidooccioni <67933711+guidooccioni@users.noreply.github.com>
2020-09-17 13:24:46 -0300

    Update Funciones.cpp

--- Funciones.cpp
+++ Funciones.cpp
@@ -1,27 +1,43 @@
 #include "LIB_TP.h"
 
-Valores_t cargaVI(Valores_t val)
+Valores_t reset(Valores_t val)
 {
-    printf("ingrese Vset:");
+	val.Vo = val.Vset;
+	val.Io = val.Vset / val.Rset;
+	return val;	
+}
+Valores_t cargaV(Valores_t val)
+{
+    printf("ingrese Vset[Volts]:");
     scanf("%f",&val.Vset); 
-    printf("ingrese Imax:");
+    val = reset(val);
+    return val;
+}
+Valores_t cargaI(Valores_t val)
+{
+	printf("\ningrese Imax[Amperes]:");
     scanf("%f",&val.Imax);
-    val.Vo = val.Vset;
-    return (val);
+    val = reset(val);
+    return val;
 }
-
-
 Valores_t cargaR(Valores_t val)
-{	val.Vo = val.Vset;
-    printf("ingrese Rset:");
+{
+    printf("ingrese Rset[Ohms]:");
     scanf("%f",&val.Rset);
-    val.Io = val.Vo / val.Rset;
-    return (val);
+    val = reset(val);
+    return val;
 }
-
 Valores_t disminuir(Valores_t val)
 {
-    Reducir(val.Vo);
-    val.Io = val.Vo / val.Rset;
-	return (val);
+    val.Vo = val.Vo * 0.8;
+	val.Io = val.Vo / val.Rset;
+	printf("\nVo:%f V\t\t\tIo:%f A\t",val.Vo,val.Io);
+	return val;
+}
+
+Valores_t estado(Valores_t val)
+{
+	while (val.Io > val.Imax)
+		val = disminuir(val);
+	return val;
 }
