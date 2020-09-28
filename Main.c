4de3c4b23c8f67d283e4634081af6a5010a65e93
guidooccioni <67933711+guidooccioni@users.noreply.github.com>
2020-09-27 22:11:46 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -6,22 +6,29 @@ int main(int argc, char** argv)
 {
 	val.Rset=1000000;
 	val.Imax = 5;
-	int sel=0;
-	while(sel!=9)
-	{printf("Seleccione magnitud:\n");
-	printf("1.Tension\n2.Ilimite\n3.Carga\n4.valoresreales\n9.salir\n\n seleccion:");
+	int menu_t=0;
+	while(menu_t!=9)
+	{printf("\nSeleccione magnitud:\n");
+	printf("1.Tension\n2.Ilimite\n3.Carga\n\n seleccion:");
 	fflush(stdin);
 	scanf("%d",&sel);
 	system ("cls");
-	if (sel ==1)
-	val = cargaV(val);
-	if (sel == 2)
-	val = cargaI(val);
-	if (sel == 3)
-	val = cargaR(val); 	
+	switch (menu_t)
+		{	
+			case Tension:
+				val = cargaV(val);
+				break;
+			case Corriente:
+				val = cargaI(val);
+				break;
+			case Resistencia:
+				val = cargaR(val); 	
+				break;
+			default:
+				break;
+		}		
 	val = estado(val);  // Función estado, compara Io con Imáx, mientras Io sea mayor q la corriente límite, llama a la función reducir 
-	printf("\nVset:%f\nVo:%f\nImax:%f\nIo:%f\nRset:%f",val.Vset,val.Vo,val.Imax,val.Io,val.Rset);
-	printf("\nSeleccion:");
+	printf("\nVset:%f\nVo:%f\nImax:%f\nIo:%f\nRset:%f\n",val.Vset,val.Vo,val.Imax,val.Io,val.Rset);
 }
 	return 0;
 }
